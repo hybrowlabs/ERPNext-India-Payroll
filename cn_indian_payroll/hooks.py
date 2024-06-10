fdf0a49d90478c9f972aeb274cd4db6827592931
shiniln-cloud <shinil.n@midocean.com>
2024-06-10 21:06:46 +0530

    module change

--- cn_indian_payroll/hooks.py
+++ cn_indian_payroll/hooks.py
@@ -228,7 +228,7 @@ app_license = "mit"
 # }
 
 fixtures = [
-    {"dt": "Custom Field", "filters": {"module": "meril"}},
+    {"dt": "Custom Field", "filters": {"module": "cn_indian_payroll"}},
 	{"dt":"Print Format","filters":{"module": "cn_indian_payroll"}}
     
 ]
