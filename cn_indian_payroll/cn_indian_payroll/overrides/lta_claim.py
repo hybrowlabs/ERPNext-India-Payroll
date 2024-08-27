f274c9712b5fb83780484f8f50964606b6e46956
shinil-cloud <shinilshinu97@example.com>
2024-08-27 08:15:14 +0530

    LTA_Claim

--- cn_indian_payroll/cn_indian_payroll/overrides/lta_claim.py
+++ cn_indian_payroll/cn_indian_payroll/overrides/lta_claim.py
@@ -1,58 +1,46 @@
 import frappe
 
-
-
-def validate(self,method):
-    amount=0
+def validate(self, method):
+    amount = 0
 
     if self.employee:
-        component=[]
+        component = []
 
-
-        lta_data=frappe.db.get_list('Salary Component',
+        lta_data = frappe.db.get_list('Salary Component',
             filters={
                 'component_type': "LTA Reimbursement",
             },
-            fields=['*'],
-            
-            )
-        if lta_data[0].name:
-            
+            fields=['*']
+        )
+
+        if lta_data and lta_data[0].name:
+
             get_salary_assignment = frappe.db.get_list('Salary Structure Assignment',
                 filters={
                     'docstatus': 1,
                     'employee': self.employee,
                 },
                 fields=['*'],
-                order_by='from_date desc'
+                order_by='from_date desc',
+                limit=1  # Fetch only the latest record
             )
-            
 
-            if get_salary_assignment[0].name:
-               
-
+            if get_salary_assignment and get_salary_assignment[0].name:
                 employee_doc = frappe.get_doc('Salary Structure Assignment', get_salary_assignment[0].name)
 
-                
-                if len(employee_doc.custom_employee_reimbursements)>0:
-                    for j in employee_doc.custom_employee_reimbursements:
-                        
-                        component.append(j.reimbursements)
+                if len(employee_doc.custom_employee_reimbursements) > 0:
+                    for reimbursement in employee_doc.custom_employee_reimbursements:
+                        component.append(reimbursement.reimbursements)
 
-        
 
-        if lta_data[0].custom_lta_component not in component:
+        if lta_data and lta_data[0].name not in component:
+           
             frappe.throw("you are not eligible for claim LTA")
-            
+    # if self.non_taxable_amount:
+    #     amount += self.non_taxable_amount
 
-    # if self.non_taxable_amount!=None:
-    #     amount+=self.non_taxable_amount
+    # if self.taxable_amount:
+    #     amount += self.taxable_amount
 
-    # if self.non_taxable_amount!=None:
-    #     amount+=self.taxable_amount
-        
-
-    # if self.amount>amount and amount!=0:
+    # if self.amount > amount and amount != 0:
     #     frappe.throw("Cannot enter the amount greater than the sum of taxable and non-taxable amounts")
-
-    
