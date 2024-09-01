7085ae94e27b26deda7bd76df67c2d22b1b509cc
shinil-cloud <shinilshinu97@example.com>
2024-09-01 10:13:00 +0530

    reworked fixtures

--- cn_indian_payroll/hooks.py
+++ cn_indian_payroll/hooks.py
@@ -238,8 +238,8 @@ fixtures = [
     {"dt": "Role", "filters": {"name": ["in", ["HR Head", "Finance Head","Payroll manager"]]}},
     {"dt":"Translation","filters":{"name":"Reimbursement Claim"}},
     # {"dt":"Employee Tax Exemption Sub Category"},
-    {"dt":"Employee Tax Exemption Category"},
-    {"dt":"Income Tax Slab"},
+    # {"dt":"Employee Tax Exemption Category"},
+    # {"dt":"Income Tax Slab"},
 
 ]
     
