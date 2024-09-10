efa3dfec4e50785201f5548fad2404658754d3d4
shinil-cloud <shinilshinu97@example.com>
2024-09-10 16:26:35 +0530

    ctc_report

--- cn_indian_payroll/cn_indian_payroll/report/ctc_breakup/ctc_breakup.py
+++ cn_indian_payroll/cn_indian_payroll/report/ctc_breakup/ctc_breakup.py
@@ -19,8 +19,13 @@ def get_all_employee(filters=None):
     reimbursement_components = {}
     data = []
 
-    ctc_components = frappe.get_all("Salary Component", filters={"custom_is_part_of_ctc": 1}, fields=["name"])
-    ctc_components_set = set(component.name for component in ctc_components)
+	ctc_components = frappe.get_all(
+		"Salary Component", 
+		filters={"custom_is_part_of_ctc": 1}, 
+		fields=["name"], 
+		order_by="custom_sequence asc"
+	)    
+	ctc_components_set = set(component.name for component in ctc_components)
 
     for each_employee in get_all_ssa:
         # Initialize row data
