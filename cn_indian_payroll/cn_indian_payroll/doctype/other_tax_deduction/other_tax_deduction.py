0114946bf8ce1f3cbe69c51d2146631ab401f354
shinil-cloud <shinilshinu97@example.com>
2024-09-11 11:52:01 +0530

    deleted_other_deduction_doctype

--- cn_indian_payroll/cn_indian_payroll/doctype/other_tax_deduction/other_tax_deduction.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Hybrowlabs technologies and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class OtherTaxDeduction(Document):
-	pass
