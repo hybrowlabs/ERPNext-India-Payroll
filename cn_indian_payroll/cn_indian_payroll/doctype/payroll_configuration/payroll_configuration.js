cfcdee48ef3d6471c17822044923ac3e493bd103
shinil-cloud <shinilshinu97@example.com>
2025-01-21 23:21:51 +0530

    payroll configuration feature

---
+++ cn_indian_payroll/cn_indian_payroll/doctype/payroll_configuration/payroll_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Hybrowlabs technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Payroll Configuration", {
+// 	refresh(frm) {
+
+// 	},
+// });
