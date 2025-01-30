7dc451f8d46312982f05f3439e1701fcc3faeee7
shinil-cloud <shinilshinu97@example.com>
2025-01-30 11:09:48 +0530

    payroll configuration

--- cn_indian_payroll/hooks.py
+++ cn_indian_payroll/hooks.py
@@ -306,10 +306,10 @@ doc_events = {
         "on_submit": "cn_indian_payroll.cn_indian_payroll.overrides.employee_promotion.on_submit",
     },
 
-    "Payroll Configuration": {
-        "validate": "cn_indian_payroll.cn_indian_payroll.overrides.payroll_configuration.validate",
+    # "Payroll Configuration": {
+    #     "validate": "cn_indian_payroll.cn_indian_payroll.overrides.payroll_configuration.validate",
         
-    },
+    # },
 
 }
 
