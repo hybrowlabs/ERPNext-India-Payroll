3046b4831c1b8e4e872530eb00d80fb7f976c5e4
shinil-cloud <shinilshinu97@gmail.com>
2025-06-11 09:35:38 +0530

    refactor: comment out unused taxable earnings calculations in CustomSalarySlip

--- cn_indian_payroll/cn_indian_payroll/overrides/salary_slip.py
+++ cn_indian_payroll/cn_indian_payroll/overrides/salary_slip.py
@@ -47,23 +47,23 @@ class CustomSalarySlip(SalarySlip):
         self.set_sub_period()
         self.apply_lop_amount_in_reimbursement_component()
 
-        self.custom_previous_taxable_earnings = (
-            self.previous_taxable_earnings_before_exemption
-        )
-        self.custom_current_taxable_earnings = (
-            self.current_structured_taxable_earnings_before_exemption
-        )
+        # self.custom_previous_taxable_earnings = (
+        #     self.previous_taxable_earnings_before_exemption
+        # )
+        # self.custom_current_taxable_earnings = (
+        #     self.current_structured_taxable_earnings_before_exemption
+        # )
 
-        self.custom_future_taxable_earnings = (
-            self.future_structured_taxable_earnings_before_exemption
-        )
-        self.custom_annual_taxable_earnings = self.ctc - (self.non_taxable_earnings)
-        if self.earnings:
-            total_ctc_taxable_amount = 0
-            for earning in self.earnings:
-                if earning.is_tax_applicable == 1:
-                    total_ctc_taxable_amount += earning.default_amount
-            self.custom_ctc_taxable_earnings = total_ctc_taxable_amount
+        # self.custom_future_taxable_earnings = (
+        #     self.future_structured_taxable_earnings_before_exemption
+        # )
+        # self.custom_annual_taxable_earnings = self.ctc - (self.non_taxable_earnings)
+        # if self.earnings:
+        #     total_ctc_taxable_amount = 0
+        #     for earning in self.earnings:
+        #         if earning.is_tax_applicable == 1:
+        #             total_ctc_taxable_amount += earning.default_amount
+        #     self.custom_ctc_taxable_earnings = total_ctc_taxable_amount
 
         self.insert_other_perquisites()
 
