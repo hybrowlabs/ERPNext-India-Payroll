f1e9e4822c2ae32dae1895b64794c69e347d5f83
shinil-cloud <shinilshinu97@gmail.com>
2025-08-22 17:42:29 +0530

    feat: Comment out previous taxable earnings calculations in CustomSalarySlip

--- cn_indian_payroll/cn_indian_payroll/overrides/salary_slip.py
+++ cn_indian_payroll/cn_indian_payroll/overrides/salary_slip.py
@@ -47,24 +47,24 @@ class CustomSalarySlip(SalarySlip):
 
 
 
-        self.custom_previous_taxable_earnings = (
-            self.previous_taxable_earnings_before_exemption
+        # self.custom_previous_taxable_earnings = (
+        #     self.previous_taxable_earnings_before_exemption
 
-        )
-        self.custom_current_taxable_earnings = (
-            self.current_structured_taxable_earnings_before_exemption
-        )
+        # )
+        # self.custom_current_taxable_earnings = (
+        #     self.current_structured_taxable_earnings_before_exemption
+        # )
 
-        self.custom_future_taxable_earnings = (
-            self.future_structured_taxable_earnings_before_exemption
-        )
-        self.custom_annual_taxable_earnings = self.ctc - (self.non_taxable_earnings)
-        if self.earnings:
-            total_ctc_taxable_amount = 0
-            for earning in self.earnings:
-                if earning.is_tax_applicable == 1:
-                    total_ctc_taxable_amount += earning.default_amount
-        self.custom_ctc_taxable_earnings = total_ctc_taxable_amount
+        # self.custom_future_taxable_earnings = (
+        #     self.future_structured_taxable_earnings_before_exemption
+        # )
+        # self.custom_annual_taxable_earnings = self.ctc - (self.non_taxable_earnings)
+        # if self.earnings:
+        #     total_ctc_taxable_amount = 0
+        #     for earning in self.earnings:
+        #         if earning.is_tax_applicable == 1:
+        #             total_ctc_taxable_amount += earning.default_amount
+        # self.custom_ctc_taxable_earnings = total_ctc_taxable_amount
 
 
 
