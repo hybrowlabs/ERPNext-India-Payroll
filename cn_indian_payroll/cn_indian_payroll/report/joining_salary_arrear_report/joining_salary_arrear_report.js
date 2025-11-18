8858c01cb42be47417a745903652c7ccaf984ac0
shinil-cloud <shinilshinu97@gmail.com>
2025-11-18 20:03:34 +0530

    feat: add Joining Salary Arrear Report with filters for employee and joining dates

---
+++ cn_indian_payroll/cn_indian_payroll/report/joining_salary_arrear_report/joining_salary_arrear_report.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2025, Hybrowlabs technologies and contributors
+// For license information, please see license.txt
+
+
+
+frappe.query_reports["Joining Salary Arrear Report"] = {
+	"filters": [
+		{
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "reqd": 1
+        },
+        {
+            "fieldname": "employee",
+            "label": __("Employee"),
+            "fieldtype": "Link",
+            "options": "Employee",
+            "reqd": 0
+        },
+		{
+            "fieldname": "joining_from",
+            "label": __("Joining From"),
+            "fieldtype": "Date",
+
+        },
+        {
+            "fieldname": "joining_to",
+            "label": __("Joining To"),
+            "fieldtype": "Date",
+
+        },
+
+	]
+};
