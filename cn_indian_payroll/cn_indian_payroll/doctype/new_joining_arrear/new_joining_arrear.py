c87e7c8977a711bd65a54e5421d98aa2db7d5107
shinil-cloud <shinilshinu97@gmail.com>
2025-11-25 16:41:35 +0530

    feat: Add working days field and update reimbursement logic in New Joining Arrear

--- cn_indian_payroll/cn_indian_payroll/doctype/new_joining_arrear/new_joining_arrear.py
+++ cn_indian_payroll/cn_indian_payroll/doctype/new_joining_arrear/new_joining_arrear.py
@@ -13,36 +13,55 @@ class NewJoiningArrear(Document):
         self.insert_additional_salary()
 
     def insert_additional_salary(self):
-        if not (self.earning_breakup or self.deduction_breakup):
+        if not (self.earning_component or self.deduction_component or self.reimbursement_component):
             return
 
         # Create Additional Salary for earnings
-        for row in self.earning_breakup:
+        for row in self.earning_component:
             additional_salary = frappe.new_doc("Additional Salary")
             additional_salary.employee = self.employee
             additional_salary.salary_component = row.salary_component
             additional_salary.amount = row.amount
             additional_salary.company = self.company
-            additional_salary.payroll_date = self.additional_salary_date
+            additional_salary.payroll_date = self.payout_date
             additional_salary.currency = "INR"
-            additional_salary.ref_doctype = "Joining Salary Arrear"
+            additional_salary.ref_doctype = "New Joining Arrear"
             additional_salary.ref_docname = self.name
             additional_salary.insert()
             additional_salary.submit()
 
         # Create Additional Salary for deductions
-        for row in self.deduction_breakup:
+        for row in self.deduction_component:
             additional_salary = frappe.new_doc("Additional Salary")
             additional_salary.employee = self.employee
             additional_salary.salary_component = row.salary_component
             additional_salary.amount = row.amount
             additional_salary.company = self.company
-            additional_salary.payroll_date = self.additional_salary_date
+            additional_salary.payroll_date = self.payout_date
             additional_salary.currency = "INR"
-            additional_salary.ref_doctype = "Joining Salary Arrear"
+            additional_salary.ref_doctype = "New Joining Arrear"
             additional_salary.ref_docname = self.name
             additional_salary.insert()
             additional_salary.submit()
+
+
+        for row in self.reimbursement_component:
+            additional_salary = frappe.new_doc("Employee Benefit Accrual")
+            additional_salary.employee = self.employee
+            additional_salary.salary_component = row.salary_component
+            additional_salary.amount = row.amount
+            additional_salary.company = self.company
+            additional_salary.payroll_period = self.payroll_period
+
+            additional_salary.benefit_accrual_date = self.posting_date
+            additional_salary.working_days = self.working_days
+            additional_salary.arrear_days = self.number_of_present_days
+            additional_salary.insert()
+            additional_salary.submit()
+
+
+
+
 
     def insert_breakup_table(self):
         if not self.employee:
@@ -67,6 +86,8 @@ class NewJoiningArrear(Document):
         salary_structure = salary_structure_assignment[0].salary_structure
         from_date = salary_structure_assignment[0].from_date
 
+        ssa=frappe.get_doc("Salary Structure Assignment",salary_structure_assignment[0].name)
+
         # Generate salary slip for preview
         new_salary_slip = make_salary_slip(
             source_name=salary_structure,
@@ -76,9 +97,37 @@ class NewJoiningArrear(Document):
             for_preview=1,
         )
 
+
+
         processed_components = []
         earning_breakup = []
         deduction_breakup = []
+
+        reimbursement_components = []
+
+
+        for accrued_benefit in ssa.custom_employee_reimbursements:
+            salary_component_doc = frappe.get_doc("Salary Component", accrued_benefit.reimbursements)
+
+
+
+            if salary_component_doc.depends_on_payment_days == 1 and salary_component_doc.custom_is_reimbursement == 1:
+                reimbursement_components.append({
+                    "salary_component": accrued_benefit.reimbursements,
+                    "amount": round(
+                        ((accrued_benefit.monthly_total_amount / 12) / new_salary_slip.total_working_days) * self.number_of_present_days
+                    ),
+                    "actual_amount":round(accrued_benefit.monthly_total_amount)
+                })
+
+            if salary_component_doc.depends_on_payment_days == 0 and salary_component_doc.custom_is_reimbursement == 1:
+                reimbursement_components.append({
+                    "salary_component": accrued_benefit.reimbursements,
+                    "amount": round(
+                        round(accrued_benefit.monthly_total_amount)
+                    ),
+                    "actual_amount":round(accrued_benefit.monthly_total_amount)
+                })
 
         for new_earning in new_salary_slip.earnings:
             component_doc = frappe.get_value(
@@ -102,6 +151,7 @@ class NewJoiningArrear(Document):
                             (new_earning.amount / new_salary_slip.total_working_days)
                             * self.number_of_present_days
                         ),
+                        "actual_amount":new_earning.amount,
                     }
                 )
                 processed_components.append(component_doc.name)
@@ -129,16 +179,24 @@ class NewJoiningArrear(Document):
                             (new_deduction.amount / new_salary_slip.total_working_days)
                             * self.number_of_present_days
                         ),
+                        "actual_amount":new_deduction.amount,
                     }
                 )
                 processed_components.append(component_doc.name)
 
-        # Assign results to document child tables
+
+
+        self.working_days=new_salary_slip.total_working_days
+
         self.set("earning_component", [])
         self.set("deduction_component", [])
+        self.set("reimbursement_component",[])
 
         for row in earning_breakup:
             self.append("earning_component", row)
 
         for row in deduction_breakup:
             self.append("deduction_component", row)
+
+        for row in reimbursement_components:
+            self.append("reimbursement_component", row)
