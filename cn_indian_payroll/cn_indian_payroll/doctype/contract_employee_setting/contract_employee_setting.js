694bc44c6e5568aa2507f6be30ab85369fd534a2
shinil-cloud <shinilshinu97@gmail.com>
2026-02-02 10:56:54 +0530

    feat: Add Company, Department, and Work Location mapping DocTypes with associated JSON and Python files

--- cn_indian_payroll/cn_indian_payroll/doctype/contract_employee_setting/contract_employee_setting.js
+++ cn_indian_payroll/cn_indian_payroll/doctype/contract_employee_setting/contract_employee_setting.js
@@ -23,7 +23,7 @@ frappe.ui.form.on("Contract Employee Setting", {
 
                 frm.refresh_field("table_peep");
 
-                console.log("Supplier options loaded:", options);
+
             }
         });
 
@@ -38,16 +38,156 @@ frappe.ui.form.on("Contract Employee Setting", {
                 }
 
                 // Build options list
+                // let options = r.message.map(d => d.name);
+                // options.unshift(""); // empty first option
+
+                // // ✅ Set options to Select field
+                // frm.set_df_property("company", "options", options.join("\n"));
+                // frm.refresh_field("company");
+
+                // console.log("Company options loaded:", options);
+
+                let options = r.message.map(d => d.name);
+                options.unshift("");
+
+                // ✅ SAFE way to set child select options
+                frm.fields_dict.map_the_company.grid.update_docfield_property(
+                    "company_in_erp",
+                    "options",
+                    options.join("\n")
+                );
+
+                frm.refresh_field("map_the_company");
+            }
+        });
+
+
+        frappe.call({
+            method: "cn_indian_payroll.cn_indian_payroll.doctype.contract_employee_setting.contract_employee_setting.get_item_tax_template",
+            callback: function (r) {
+
+                if (!r.message || !r.message.length) {
+                    frappe.msgprint("No Item Tax Template found");
+                    return;
+                }
+
                 let options = r.message.map(d => d.name);
                 options.unshift(""); // empty first option
 
                 // ✅ Set options to Select field
-                frm.set_df_property("company", "options", options.join("\n"));
-                frm.refresh_field("company");
+                frm.set_df_property("item_tax_template", "options", options.join("\n"));
+                frm.refresh_field("item_tax_template");
 
-                console.log("Company options loaded:", options);
+
+                // let options = r.message.map(d => d.name);
+                // options.unshift("");
+
+                // // ✅ SAFE way to set child select options
+                // frm.fields_dict.map_the_department.grid.update_docfield_property(
+                //     "department_in_erp",
+                //     "options",
+                //     options.join("\n")
+                // );
+
+                // frm.refresh_field("map_the_department");
+
             }
         });
+
+        frappe.call({
+            method: "cn_indian_payroll.cn_indian_payroll.doctype.contract_employee_setting.contract_employee_setting.get_payment_terms_template",
+            callback: function (r) {
+
+                if (!r.message || !r.message.length) {
+                    frappe.msgprint("No Payment Terms Template found");
+                    return;
+                }
+
+                let options = r.message.map(d => d.name);
+                options.unshift(""); // empty first option
+
+                // ✅ Set options to Select field
+                frm.set_df_property("payment_terms_template", "options", options.join("\n"));
+                frm.refresh_field("payment_terms_template");
+
+
+
+                // let options = r.message.map(d => d.name);
+                // options.unshift("");
+
+                // // ✅ SAFE way to set child select options
+                // frm.fields_dict.map_the_work_location.grid.update_docfield_property(
+                //     "location_in_erp",
+                //     "options",
+                //     options.join("\n")
+                // );
+
+                // frm.refresh_field("map_the_work_location");
+
+            }
+        });
+
+
+
+
+        frappe.call({
+            method: "cn_indian_payroll.cn_indian_payroll.doctype.contract_employee_setting.contract_employee_setting.get_department_list",
+            callback: function (r) {
+
+                if (!r.message || !r.message.length) {
+                    frappe.msgprint("No Department found");
+                    return;
+                }
+
+
+
+
+                let options = r.message.map(d => d.name);
+                options.unshift("");
+
+                // ✅ SAFE way to set child select options
+                frm.fields_dict.map_the_department.grid.update_docfield_property(
+                    "department_in_erp",
+                    "options",
+                    options.join("\n")
+                );
+
+                frm.refresh_field("map_the_department");
+
+            }
+        });
+
+
+
+        frappe.call({
+            method: "cn_indian_payroll.cn_indian_payroll.doctype.contract_employee_setting.contract_employee_setting.get_worklocation_list",
+            callback: function (r) {
+
+                if (!r.message || !r.message.length) {
+                    frappe.msgprint("No Work Location found");
+                    return;
+                }
+
+
+
+
+                let options = r.message.map(d => d.name);
+                options.unshift("");
+
+                // ✅ SAFE way to set child select options
+                frm.fields_dict.map_the_work_location.grid.update_docfield_property(
+                    "location_in_erp",
+                    "options",
+                    options.join("\n")
+                );
+
+                frm.refresh_field("map_the_work_location");
+
+            }
+        });
+
+
+
 
     }
 });
--- cn_indian_payroll/cn_indian_payroll/doctype/contract_employee_setting/contract_employee_setting.py
+++ cn_indian_payroll/cn_indian_payroll/doctype/contract_employee_setting/contract_employee_setting.py
@@ -89,3 +89,151 @@ def get_company_list():
     data = resp.json().get("data", [])
 
     return data
+
+
+
+
+@frappe.whitelist()
+def get_item_tax_template():
+
+    settings = frappe.get_single("Integration Settings")
+
+    if not settings.url or not settings.api_key or not settings.api_secret:
+        frappe.throw("Integration Settings is incomplete")
+
+    url = urljoin(settings.url, "/api/resource/Item Tax Template")
+
+
+    headers = {
+        "Authorization": f"token {settings.api_key}:{settings.api_secret}",
+        "Accept": "application/json"
+    }
+
+    params = {
+        "limit_page_length": 20
+    }
+
+
+    try:
+        resp = requests.get(url, headers=headers, params=params, timeout=20)
+    except Exception as e:
+        frappe.throw(f"Connection failed: {str(e)}")
+
+
+    if resp.status_code != 200:
+        frappe.throw(f"Company API failed: {resp.text}")
+
+
+    data = resp.json().get("data", [])
+
+    return data
+
+
+@frappe.whitelist()
+def get_payment_terms_template():
+
+    settings = frappe.get_single("Integration Settings")
+
+    if not settings.url or not settings.api_key or not settings.api_secret:
+        frappe.throw("Integration Settings is incomplete")
+
+    url = urljoin(settings.url, "/api/resource/Payment Terms Template")
+
+
+    headers = {
+        "Authorization": f"token {settings.api_key}:{settings.api_secret}",
+        "Accept": "application/json"
+    }
+
+    params = {
+        "limit_page_length": 20
+    }
+
+
+    try:
+        resp = requests.get(url, headers=headers, params=params, timeout=20)
+    except Exception as e:
+        frappe.throw(f"Connection failed: {str(e)}")
+
+
+    if resp.status_code != 200:
+        frappe.throw(f"Company API failed: {resp.text}")
+
+
+    data = resp.json().get("data", [])
+
+    return data
+
+
+
+@frappe.whitelist()
+def get_department_list():
+
+    settings = frappe.get_single("Integration Settings")
+
+    if not settings.url or not settings.api_key or not settings.api_secret:
+        frappe.throw("Integration Settings is incomplete")
+
+    url = urljoin(settings.url, "/api/resource/Department")
+
+
+    headers = {
+        "Authorization": f"token {settings.api_key}:{settings.api_secret}",
+        "Accept": "application/json"
+    }
+
+    params = {
+        "limit_page_length": 20
+    }
+
+
+    try:
+        resp = requests.get(url, headers=headers, params=params, timeout=20)
+    except Exception as e:
+        frappe.throw(f"Connection failed: {str(e)}")
+
+
+    if resp.status_code != 200:
+        frappe.throw(f"Company API failed: {resp.text}")
+
+
+    data = resp.json().get("data", [])
+
+    return data
+
+
+
+@frappe.whitelist()
+def get_worklocation_list():
+
+    settings = frappe.get_single("Integration Settings")
+
+    if not settings.url or not settings.api_key or not settings.api_secret:
+        frappe.throw("Integration Settings is incomplete")
+
+    url = urljoin(settings.url, "/api/resource/Branch")
+
+
+    headers = {
+        "Authorization": f"token {settings.api_key}:{settings.api_secret}",
+        "Accept": "application/json"
+    }
+
+    params = {
+        "limit_page_length": 0
+    }
+
+
+    try:
+        resp = requests.get(url, headers=headers, params=params, timeout=20)
+    except Exception as e:
+        frappe.throw(f"Connection failed: {str(e)}")
+
+
+    if resp.status_code != 200:
+        frappe.throw(f"Company API failed: {resp.text}")
+
+
+    data = resp.json().get("data", [])
+
+    return data
