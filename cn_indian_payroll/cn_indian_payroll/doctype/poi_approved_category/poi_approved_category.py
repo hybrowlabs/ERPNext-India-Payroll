2a8c602f524e12a7a6c7e2f5484ec2788720fa82
shinil-cloud <shinilshinu97@gmail.com>
2026-02-11 09:36:33 +0530

    feat: update POI Approved Category validation and handling for approved/rejected statuses

--- cn_indian_payroll/cn_indian_payroll/doctype/poi_approved_category/poi_approved_category.py
+++ cn_indian_payroll/cn_indian_payroll/doctype/poi_approved_category/poi_approved_category.py
@@ -1,9 +1,31 @@
 # Copyright (c) 2026, Hybrowlabs technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class POIApprovedCategory(Document):
-	pass
+	def validate(self):
+		if self.proof_id and self.status == "Approved":
+			existing_approved_category = frappe.get_doc(
+				"Employee Tax Exemption Proof Submission",self.proof_id
+				
+			)
+			if existing_approved_category.tax_exemption_proofs:
+				for category in existing_approved_category.tax_exemption_proofs:
+					if category.exemption_sub_category == self.exemption_sub_category:
+						category.custom_proof_status = "Approved"
+			existing_approved_category.save()
+
+		if self.proof_id and self.status == "Rejected":
+			existing_approved_category = frappe.get_doc(
+				"Employee Tax Exemption Proof Submission",self.proof_id
+				
+			)
+			if existing_approved_category.tax_exemption_proofs:
+				for category in existing_approved_category.tax_exemption_proofs:
+					if category.exemption_sub_category == self.exemption_sub_category:
+						category.custom_proof_status = "Rejected"
+			existing_approved_category.save()
+						
