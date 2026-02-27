4256467702c9aca2e826eff062702906d93d5f0f
shinil-cloud <shinilshinu97@gmail.com>
2026-02-27 12:28:48 +0530

    fix:declaration approved category

--- cn_indian_payroll/cn_indian_payroll/doctype/declaration_approved_category/declaration_approved_category.py
+++ cn_indian_payroll/cn_indian_payroll/doctype/declaration_approved_category/declaration_approved_category.py
@@ -1,9 +1,25 @@
-# Copyright (c) 2026, Hybrowlabs technologies and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class DeclarationApprovedCategory(Document):
-	pass
+
+    def validate(self):
+
+        if self.status in ["Approved", "Rejected"] and self.declaration_id:
+
+            declaration = frappe.get_doc(
+                "Employee Tax Exemption Declaration",
+                self.declaration_id
+            )
+
+            for row in declaration.declarations:
+
+                if row.exemption_sub_category == self.exemption_sub_category:
+
+                    frappe.db.set_value(
+                        row.doctype,   # child doctype
+                        row.name,      # row ID
+                        "custom_status",
+                        self.status
+                    )
--- cn_indian_payroll/cn_indian_payroll/overrides/webapp_api/tds_projection.py
+++ cn_indian_payroll/cn_indian_payroll/overrides/webapp_api/tds_projection.py
@@ -4169,10 +4169,10 @@ def get_employee_declaration_investments(employee=None, company=None, payroll_pe
 
     if net_taxable_income <= (tax_slab_doc.custom_taxable_income_is_less_than or 0):
     
-    rebate = min(
-        income_tax_on_net_taxable_income,
-        tax_slab_doc.custom_maximum_amount or 0
-    )
+        rebate = min(
+            income_tax_on_net_taxable_income,
+            tax_slab_doc.custom_maximum_amount or 0
+        )
     else:
         rebate = 0
 
