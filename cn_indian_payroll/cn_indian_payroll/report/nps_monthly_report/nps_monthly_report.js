88781bbc088c1b0cbae0aedb1d5654fb76883ce5
04anilr <anilr5364@gmail.com>
2026-03-24 10:43:07 +0530

    feat: create report in cn indian payroll

---
+++ cn_indian_payroll/cn_indian_payroll/report/nps_monthly_report/nps_monthly_report.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2026, Hybrowlabs technologies and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["NPS Monthly Report"] = {
+	"filters": [
+        {
+            fieldname: "employee",
+            label: "Employee",
+            fieldtype: "Link",
+            options: "Employee"
+        },
+        {
+            fieldname: "department",
+            label: "Department",
+            fieldtype: "Link",
+            options: "Department"
+        },
+        {
+            fieldname: "designation",
+            label: "Designation",
+            fieldtype: "Link",
+            options: "Designation"
+        }
+	]
+};
